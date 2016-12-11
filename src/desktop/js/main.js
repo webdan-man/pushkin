3806ad93bc13f8198fee748595bebfc1c4487bbd
webdan-man <i.const.yank@gmail.com>
2016-12-12 01:22:26 +0200

    desktop trabl

--- src/desktop/js/main.js
+++ src/desktop/js/main.js
@@ -1,114 +1,249 @@
-//карта
-! function(e, t, n) {
-    function r() {
-        for (; u[0] && "loaded" == u[0][l];) o = u.shift(), o[f] = !a.parentNode.insertBefore(o, a)
-    }
-    for (var i, s, o, u = [], a = e.scripts[0], f = "onreadystatechange", l = "readyState"; i = n.shift();) s = e.createElement(t), "async" in a ? (s.async = !1, e.head.appendChild(s)) : a[l] ? (u.push(s), s[f] = r) : e.write("<" + t + ' src="' + i + '" defer></' + t + ">"), s.src = i
-}(document, "script", ["https://maps.googleapis.com/maps/api/js?key=AIzaSyAdzwz73OHrdVGjKVKow8ID8T31yNxiBSI", "js/map.js"]);
+;
+$(document).ready(function() {
 
-// фіксоване меню
-$(window).scroll(function() {
-    hfixed();
-});
-
-function hfixed() {
-    if ($(window).scrollTop() > 196) {
-        $('.header').addClass('h-fixed');
-    } else {
-        $('.header').removeClass('h-fixed');
-    }
-};
-
-//скролл меню
-$('.mena').click(function(e) {
-    e.preventDefault();
-    $("html, body").animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    }, 2000);
-});
-
-/*
-
-//гео, валидация
-function getURLParameter(name) {return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;} 
-    function run_geo(geo_url){
-        $.ajax({type: 'GET',url: geo_url,dataType: 'xml',
-            success: function(xml) {$(xml).find('ip').each(function(){
-            var city = $(this).find('city').text();
-            var region = $(this).find('region').text();
-            if(city!=region){var ipg = city+', '+region;}else{var ipg = city;}
-            $('<input type="hidden" />').attr({name: 'location', class: 'location', value:ipg}).appendTo("form");
-        });}});
-    }
-    $.get("http://ipinfo.io", function(response) {geo_url='http://ipgeobase.ru:7020/geo?ip='+response.ip; run_geo(geo_url);}, "jsonp");
-    utm=[];$.each(["utm_source","utm_medium","utm_campaign","utm_term",'source_type','source','position_type','position','added','creative','matchtype'],function(i,v){$('<input type="hidden" />').attr({name: v, class: v, value: function(){if(getURLParameter(v) == undefined)return '-'; else return getURLParameter(v)}}).appendTo("form")});
-    $('<input type="hidden" />').attr({name: 'url', value: document.location.href}).appendTo("form");
-    $('<input type="hidden" />').attr({name: 'title', value: document.title}).appendTo("form");
-
-$('input[name="name"]').blur(function() {if($(this).val().length < 2) {$(this).addClass('error-input');}});
-$('input[name="name"]').focus(function() {$(this).removeClass('error-input');});
-
-$('input[name="phone"]').mask('+7 (999) 999-99-99');
-$('input[name="phone"]').blur(function() {if($(this).val().length != 18) {$(this).addClass('error-input');}});
-$('input[name="phone"]').focus(function() {$(this).removeClass('error-input');});
-
-function validateEmail(email) {var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;return re.test(email);};
-
-$('input[name="email"]').blur(function() {if(!validateEmail($(this).val())) {$(this).addClass('error-input');}});
-$('input[name="email"]').focus(function() {$(this).removeClass('error-input');});
-
-//отправка формы
-$('form').submit(function(e){
-        e.preventDefault();
-        $(this).find('input[type="text"]').trigger('blur');
-        if(!$(this).find('input[type="text"]').hasClass('error-input')){
-            var type=$(this).attr('method');
-            var url=$(this).attr('action');
-            var data=$(this).serialize();
-            var track_event=$(this).find('input[name="event"]').val();
-            $.ajax({type: type, url: url, data: data,
-                success : function(){
-                    $.arcticmodal('close');$('#sps').arcticmodal();
-                    //submit_track_event(track_event);
-                }
-            }); 
-        }else{
-
-            var eror_pop_text = '';
-
-            if ($(this).find('input[name="name"]').hasClass('error-input') && !$(this).find('input[name="phone"]').hasClass('error-input')) {
-                eror_pop_text = 'Пожалуйста введите имя';
-            } else
-
-            if($(this).find('input[name="phone"]').hasClass('error-input') && !$(this).find('input[name="name"]').hasClass('error-input')){
-                eror_pop_text = 'Пожалуйста введите телефон';
-            }else
-
-            if($(this).find('input[name="phone"]').hasClass('error-input') && $(this).find('input[name="name"]').hasClass('error-input')){
-                eror_pop_text = 'Пожалуйста введите имя и телефон';
-            }
-
-            $('#form-error-text').html(eror_pop_text)
-            $('#form-error-pop').arcticmodal();
+    //карта
+    ! function(e, t, n) {
+        function r() {
+            for (; u[0] && "loaded" == u[0][l];) o = u.shift(), o[f] = !a.parentNode.insertBefore(o, a)
         }
+        for (var i, s, o, u = [], a = e.scripts[0], f = "onreadystatechange", l = "readyState"; i = n.shift();) s = e.createElement(t), "async" in a ? (s.async = !1, e.head.appendChild(s)) : a[l] ? (u.push(s), s[f] = r) : e.write("<" + t + ' src="' + i + '" defer></' + t + ">"), s.src = i
+    }(document, "script", ["https://maps.googleapis.com/maps/api/js?key=AIzaSyAdzwz73OHrdVGjKVKow8ID8T31yNxiBSI", "js/map.js"]);
+
+    // фіксоване меню
+    $(window).scroll(function() {
+        hfixed();
     });
-*/
 
-//слайдери
+    function hfixed() {
+        if ($(window).scrollTop() > 196) {
+            $('.header').addClass('h-fixed');
+        } else {
+            $('.header').removeClass('h-fixed');
+        }
+    };
+
+    //скролл меню
+    $('.mena').click(function(e) {
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 2000);
+    });
+
+    /*
+
+    //гео, валидация
+    function getURLParameter(name) {return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;} 
+        function run_geo(geo_url){
+            $.ajax({type: 'GET',url: geo_url,dataType: 'xml',
+                success: function(xml) {$(xml).find('ip').each(function(){
+                var city = $(this).find('city').text();
+                var region = $(this).find('region').text();
+                if(city!=region){var ipg = city+', '+region;}else{var ipg = city;}
+                $('<input type="hidden" />').attr({name: 'location', class: 'location', value:ipg}).appendTo("form");
+            });}});
+        }
+        $.get("http://ipinfo.io", function(response) {geo_url='http://ipgeobase.ru:7020/geo?ip='+response.ip; run_geo(geo_url);}, "jsonp");
+        utm=[];$.each(["utm_source","utm_medium","utm_campaign","utm_term",'source_type','source','position_type','position','added','creative','matchtype'],function(i,v){$('<input type="hidden" />').attr({name: v, class: v, value: function(){if(getURLParameter(v) == undefined)return '-'; else return getURLParameter(v)}}).appendTo("form")});
+        $('<input type="hidden" />').attr({name: 'url', value: document.location.href}).appendTo("form");
+        $('<input type="hidden" />').attr({name: 'title', value: document.title}).appendTo("form");
+
+    $('input[name="name"]').blur(function() {if($(this).val().length < 2) {$(this).addClass('error-input');}});
+    $('input[name="name"]').focus(function() {$(this).removeClass('error-input');});
+
+    $('input[name="phone"]').mask('+7 (999) 999-99-99');
+    $('input[name="phone"]').blur(function() {if($(this).val().length != 18) {$(this).addClass('error-input');}});
+    $('input[name="phone"]').focus(function() {$(this).removeClass('error-input');});
+
+    function validateEmail(email) {var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;return re.test(email);};
+
+    $('input[name="email"]').blur(function() {if(!validateEmail($(this).val())) {$(this).addClass('error-input');}});
+    $('input[name="email"]').focus(function() {$(this).removeClass('error-input');});
+
+    //отправка формы
+    $('form').submit(function(e){
+            e.preventDefault();
+            $(this).find('input[type="text"]').trigger('blur');
+            if(!$(this).find('input[type="text"]').hasClass('error-input')){
+                var type=$(this).attr('method');
+                var url=$(this).attr('action');
+                var data=$(this).serialize();
+                var track_event=$(this).find('input[name="event"]').val();
+                $.ajax({type: type, url: url, data: data,
+                    success : function(){
+                        $.arcticmodal('close');$('#sps').arcticmodal();
+                        //submit_track_event(track_event);
+                    }
+                }); 
+            }else{
+
+                var eror_pop_text = '';
+
+                if ($(this).find('input[name="name"]').hasClass('error-input') && !$(this).find('input[name="phone"]').hasClass('error-input')) {
+                    eror_pop_text = 'Пожалуйста введите имя';
+                } else
+
+                if($(this).find('input[name="phone"]').hasClass('error-input') && !$(this).find('input[name="name"]').hasClass('error-input')){
+                    eror_pop_text = 'Пожалуйста введите телефон';
+                }else
+
+                if($(this).find('input[name="phone"]').hasClass('error-input') && $(this).find('input[name="name"]').hasClass('error-input')){
+                    eror_pop_text = 'Пожалуйста введите имя и телефон';
+                }
+
+                $('#form-error-text').html(eror_pop_text)
+                $('#form-error-pop').arcticmodal();
+            }
+        });
+    */
+
+    //слайдери
+
+    slider1 = $('#sld1').bxSlider({
+        infiniteLoop: true,
+        pager: false,
+        pagerCustom: false,
+        controls: false,
+        auto: true,
+        speed: 400
+    });
+    $('.sec1 .prev').click(function(e) {
+        e.preventDefault();
+        slider1.goToPrevSlide();
+    });
+    $('.sec1 .next').click(function(e) {
+        e.preventDefault();
+        slider1.goToNextSlide();
+    });
+
+    slider2 = $('.sec2 .slidw .slider#sld2').bxSlider({
+        infiniteLoop: true,
+        controls: false,
+        pager: false,
+        speed: 500,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 2,
+        moveSlides: 1,
+        slideWidth: 470,
+        onSlideNext: function($slideElement, oldIndex, newIndex) {
+            //$('.rewievs .sliderw #wrap-sld2 .slide').addClass('fadeouted');
+            $('.sec2 .slidw .slider#sld2 .slid').removeClass('active');
+            //$('.rewievs .sliderw #wrap-sld2 .slide[data-sld="'+newIndex+'"]').removeClass('fadeouted');
+            $('.sec2 .slidw .slider#sld2 .slid[data-sld="' + (oldIndex - 1) + '"]').removeClass('active');
+            $('.sec2 .slidw .slider#sld2 .slid[data-sld="' + newIndex + '"]').addClass('active');
+            $('.sec2 .slidw .slider#sld2 .slid[data-sld="' + (newIndex + 1) + '"]').addClass('active');
+        },
+        onSlidePrev: function($slideElement, oldIndex, newIndex) {
+            //$('.rewievs .sliderw #wrap-sld2 .slide').addClass('fadeouted');
+            $('.sec2 .slidw .slider#sld2 .slid').removeClass('active');
+            //$('.rewievs .sliderw #wrap-sld2 .slide[data-sld="'+newIndex+'"]').removeClass('fadeouted');
+            $('.sec2 .slidw .slider#sld2 .slid[data-sld="' + oldIndex + '"]').addClass('active');
+            $('.sec2 .slidw .slider#sld2 .slid[data-sld="' + (oldIndex + 1) + '"]').removeClass('active');
+            $('.sec2 .slidw .slider#sld2 .slid[data-sld="' + newIndex + '"]').addClass('active');
+        },
+        onSliderLoad: function() {
+            $('.sec2 .slidw .slider#sld2 .slid.active.bx-clone').removeClass('active');
+            //$('.rewievs .sliderw #wrap-sld2 .slide').addClass('fadeouted');
+            //$('.rewievs .sliderw #wrap-sld2 .slide.active').removeClass('fadeouted');
+            $('.sec2').addClass('loaded-slider');
+        }
+
+    });
+
+    $('.sec2 .prev').click(function(e) {
+        e.preventDefault();
+        slider2.goToPrevSlide();
+    });
+
+    $('.sec2 .next').click(function(e) {
+        e.preventDefault();
+        slider2.goToNextSlide();
+    });
 
 
-slider1 = $('#sld1').bxSlider({
-    pager: true,
-    pagerCustom: false,
-    controls: false,
-    auto: false,
-    speed: 400
-});
-$('.sec1 .prev').click(function(e) {
-    e.preventDefault();
-    slider1.goToPrevSlide();
-});
-$('.sec1 .next').click(function(e) {
-    e.preventDefault();
-    slider1.goToNextSlide();
+    slider3 = $('#sld3').bxSlider({
+        infiniteLoop: true,
+        pager: true,
+        pagerCustom: '#pgc3',
+        controls: false,
+        auto: true,
+        speed: 400
+    });
+    slider4 = $('#sld4').bxSlider({
+        infiniteLoop: true,
+        pager: true,
+        pagerCustom: '#pgc4',
+        controls: false,
+        auto: true,
+        speed: 400
+    });
+    slider5 = $('#sld5').bxSlider({
+        infiniteLoop: true,
+        pager: true,
+        pagerCustom: '#pgc5',
+        controls: false,
+        auto: true,
+        speed: 400
+    });
+    slider6 = $('#sld6').bxSlider({
+        infiniteLoop: true,
+        pager: true,
+        pagerCustom: '#pgc6',
+        pagerType: 'short',
+        controls: false,
+        auto: true,
+        speed: 400
+    });
+    $('.sec6 .prev').click(function(e) {
+        e.preventDefault();
+        slider6.goToPrevSlide();
+    });
+
+    $('.sec6 .next').click(function(e) {
+        e.preventDefault();
+        slider6.goToNextSlide();
+    });
+
+    slider7 = $('.sec7 .slidw .slider#sld7').bxSlider({
+        infiniteLoop: true,
+        controls: false,
+        pager: false,
+        speed: 500,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1,
+        slideWidth: 940,
+        onSlideNext: function($slideElement, oldIndex, newIndex) {
+            //$('.rewievs .sliderw #wrap-sld2 .slide').addClass('fadeouted');
+            $('.sec7 .slidw .slider#sld7 .slid').removeClass('active');
+            //$('.rewievs .sliderw #wrap-sld2 .slide[data-sld="'+newIndex+'"]').removeClass('fadeouted');
+            $('.sec7 .slidw .slider#sld7 .slid[data-sld="' + newIndex + '"]').addClass('active');
+        },
+        onSlidePrev: function($slideElement, oldIndex, newIndex) {
+            //$('.rewievs .sliderw #wrap-sld2 .slide').addClass('fadeouted');
+            $('.sec7 .slidw .slider#sld7 .slid').removeClass('active');
+            //$('.rewievs .sliderw #wrap-sld2 .slide[data-sld="'+newIndex+'"]').removeClass('fadeouted');
+            $('.sec7 .slidw .slider#sld7 .slid[data-sld="' + newIndex + '"]').addClass('active');
+        },
+        onSliderLoad: function() {
+            $('.sec7 .slidw .slider#sld7 .slid.active.bx-clone').removeClass('active');
+            //$('.rewievs .sliderw #wrap-sld2 .slide').addClass('fadeouted');
+            //$('.rewievs .sliderw #wrap-sld2 .slide.active').removeClass('fadeouted');
+            $('.sec7').addClass('loaded-slider');
+        }
+
+    });
+
+    $('.sec7 .prev').click(function(e) {
+        e.preventDefault();
+        slider7.goToPrevSlide();
+    });
+
+    $('.sec7 .next').click(function(e) {
+        e.preventDefault();
+        slider7.goToNextSlide();
+    });
+
 });
